fb005327a1b109480031fc4734273a9e4fd1ce3d
dev-yun <yunpro2766@gmail.com>
2022-11-30 22:44:41 +0900

    Feat: 홈페이지 구현

---
+++ diary/src/components/DiaryList/DiaryItem.jsx
@@ -0,0 +1,121 @@
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled, { css } from 'styled-components';
+import { DiaryDispatchContext } from '../../App';
+import Button from '../../common/Button/Button';
+import { slEllipsis } from '../../styles/util';
+
+const getWrapperBackgroundColor = emotion => {
+  let wrapperBackground;
+
+  switch (emotion) {
+    case 1:
+      wrapperBackground = '#64c964';
+      break;
+    case 2:
+      wrapperBackground = '#9dd772';
+      break;
+    case 3:
+      wrapperBackground = '#fdce17';
+      break;
+    case 4:
+      wrapperBackground = '#fd8446';
+      break;
+    case 5:
+      wrapperBackground = '#fd565f';
+      break;
+    default:
+      wrapperBackground = '#ececec';
+  }
+  return css`
+    background-color: ${wrapperBackground};
+  `;
+};
+
+const StyledItem = styled.li`
+  display: flex;
+  justify-content: space-between;
+
+  padding: 15px 0px;
+  border-bottom: 1px solid #ececec;
+`;
+
+const ImgWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+
+  min-width: 120px;
+  height: 80px;
+  border-radius: 5px;
+
+  ${props => getWrapperBackgroundColor(props.type)}
+`;
+
+const ContentWrapper = styled.div`
+  ${slEllipsis}
+  cursor: pointer;
+  flex-grow: 1;
+
+  padding-top: 5px;
+  padding-left: 10px;
+  padding-right: 10px;
+  box-sizing: border-box;
+
+  p:first-child {
+    font-weight: bold;
+    font-size: 1.1rem;
+
+    margin-bottom: 10px;
+  }
+  p {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    padding-bottom: 2px;
+  }
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  gap: 5px;
+`;
+
+function DiaryItem({ id, content, emotion, date }) {
+  const navigate = useNavigate();
+  const strDate = new Date(+date).toLocaleDateString();
+  const { onRemove } = useContext(DiaryDispatchContext);
+
+  const goDiaryDetail = () => {
+    navigate(`/diary/${id}`);
+  };
+
+  const goDiaryEdit = () => {
+    navigate(`/edit/${id}`);
+  };
+
+  const handleRemove = () => {
+    onRemove(id);
+  };
+
+  return (
+    <StyledItem>
+      <ImgWrapper type={emotion} onClick={goDiaryDetail}>
+        <img
+          src={`${process.env.PUBLIC_URL}/assets/emotion/emotion${emotion}.png`}
+          alt="emotionImg"
+        />
+      </ImgWrapper>
+      <ContentWrapper onClick={goDiaryDetail}>
+        <p>{strDate}</p>
+        <p>{content}</p>
+      </ContentWrapper>
+      <ButtonWrapper>
+        <Button type="positive" text="수정하기" onClick={goDiaryEdit} />
+        <Button type="negative" text="삭제하기" onClick={handleRemove} />
+      </ButtonWrapper>
+    </StyledItem>
+  );
+}
+
+export default DiaryItem;
--- diary/src/pages/Home.jsx
+++ diary/src/pages/Home.jsx
@@ -1,8 +1,71 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { DiaryStateContext } from '../App';
+
 import Header from '../common/Header/Header';
+import DiaryList from '../components/DiaryList/DiaryList';
 
 function Home() {
-  return <Header headText="HOME 화면" />;
+  const diaryList = useContext(DiaryStateContext);
+
+  const [data, setData] = useState([]);
+  const [curDate, setCurDate] = useState(new Date());
+  const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`;
+
+  useEffect(() => {
+    if (diaryList.length <= 1) {
+      return;
+    }
+    const firstDay = new Date(
+      curDate.getFullYear(),
+      curDate.getMonth(),
+      1,
+    ).getTime();
+
+    const lastDay = new Date(
+      curDate.getFullYear(),
+      curDate.getMonth() + 1,
+      0,
+    ).getTime();
+
+    setData(
+      diaryList.filter(item => firstDay <= item.date && item.date <= lastDay),
+    );
+  }, [curDate, diaryList]);
+
+  const increaseMonth = () => {
+    setCurDate(
+      new Date(
+        curDate.getFullYear(),
+        curDate.getMonth() + 1,
+        curDate.getDate(),
+      ),
+    );
+  };
+
+  const decreaseMonth = () => {
+    setCurDate(
+      new Date(
+        curDate.getFullYear(),
+        curDate.getMonth() - 1,
+        curDate.getDate(),
+      ),
+    );
+  };
+
+  return (
+    <>
+      <Header
+        headText={headText}
+        leftBtnText="<"
+        rightBtnText=">"
+        leftBtnClick={decreaseMonth}
+        rightBtnClick={increaseMonth}
+      />
+      <main>
+        <DiaryList diaryList={data} />
+      </main>
+    </>
+  );
 }
 
 export default Home;
